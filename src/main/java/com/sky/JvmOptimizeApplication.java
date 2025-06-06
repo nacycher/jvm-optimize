7e915c95b1eced637b3c7b686bd91b47db15c715
nacycher <nacycher@163.com>
2025-06-06 16:51:59 +0800

    init project

---
+++ src/main/java/com/sky/JvmOptimizeApplication.java
@@ -0,0 +1,10 @@
+package com.sky;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmOptimizeApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/sky/JvmOptimizeApplication.java
@@ -0,0 +1,12 @@
+package com.sky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JvmOptimizeApplication {
+
+    @Test
+    void contextLoads() {
+    }
+}
